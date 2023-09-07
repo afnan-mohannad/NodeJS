41eaac16974f054e10a171e0db5b3dc83d978fec
Afnan Mohannad <142498440+afnan-mohannad@users.noreply.github.com>
2023-09-07 12:26:32 +0300

    Create bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+
+const bubbleSort = (arr) => {
+  for (var i = 0; i < arr.length; i++) {
+  
+        for (var j = 0; j < (arr.length - i - 1); j++) {
+  
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+  
+    console.log(arr);
+};
+
+console.log(bubbleSort([2, 1, 3])); 
